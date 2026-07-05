b2ef99b7bf778229cbca916f8c829577ebef642b
Priyanshu Tiwari <tiwari9554priyanshu@gmail.com>
2026-07-05 18:14:44 +0530

    add js in form and makeing it dynamic

--- js/script.js
+++ js/script.js
@@ -4,3 +4,63 @@ const navLinks = document.querySelector(".nav-links");
 menuBtn.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+
+const form=document.getElementById("contactForm");
+
+const name=document.getElementById("name");
+
+const email=document.getElementById("email");
+
+const subject=document.getElementById("subject");
+
+const message=document.getElementById("message");
+
+const sendBtn=document.getElementById("sendBtn");
+
+form.addEventListener("submit",function(e){
+
+e.preventDefault();
+
+});
+
+if(
+
+name.value==="" ||
+
+email.value==="" ||
+
+subject.value==="" ||
+
+message.value===""
+
+){
+
+alert("Please fill all fields");
+
+return;
+
+}
+
+if(
+
+!email.value.includes("@")
+
+){
+
+alert("Invalid Email");
+
+return;
+
+}
+
+sendBtn.innerHTML="Sending...";
+
+setTimeout(()=>{
+
+alert("Message Sent Successfully");
+
+sendBtn.innerHTML="Send Message";
+
+form.reset();
+
+},2000);

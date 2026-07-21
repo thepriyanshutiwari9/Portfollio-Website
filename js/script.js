2333543f987135e8fef0913af2bc6b1953705fd3
Priyanshu Tiwari <tiwari9554priyanshu@gmail.com>
2026-07-21 09:49:50 +0530

    Frontend part one completed

--- js/script.js
+++ js/script.js
@@ -40,3 +40,70 @@ form.addEventListener("submit", function (e) {
     }, 2000);
 
 });
+
+const topBtn=document.getElementById("topBtn");
+
+window.addEventListener("scroll",()=>{
+
+    if(window.scrollY>300){
+
+        topBtn.style.display="block";
+
+    }
+
+    else{
+
+        topBtn.style.display="none";
+
+    }
+
+});
+
+topBtn.addEventListener("click",()=>{
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+});
+
+
+const sections=document.querySelectorAll("section");
+
+const navLinks=document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll",()=>{
+
+    let current="";
+
+    sections.forEach(section=>{
+
+        const sectionTop=section.offsetTop-150;
+
+        const sectionHeight=section.offsetHeight;
+
+        if(window.scrollY>=sectionTop){
+
+            current=section.getAttribute("id");
+
+        }
+
+    });
+
+    navLinks.forEach(link=>{
+
+        link.classList.remove("active");
+
+        if(link.getAttribute("href")==="#"+current){
+
+            link.classList.add("active");
+
+        }
+
+    });
+
+});
